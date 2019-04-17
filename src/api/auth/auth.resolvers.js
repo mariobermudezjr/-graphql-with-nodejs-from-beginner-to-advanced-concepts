f9eba633e1e4c3f738781025492986d2e512e740
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2019-04-17 15:02:08 -0700

    [feature] Add login mutation with  email and password

--- src/api/auth/auth.resolvers.js
+++ src/api/auth/auth.resolvers.js
@@ -29,8 +29,39 @@ async function signup(_, { input }, ctx, info) {
   };
 }
 
+async function login(_, { input }, ctx, info) {
+  const { value, error } = validateUser(input);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  const user = await ctx.models.auth.findOne({ email: value.email });
+
+  if (!user) {
+    throw new Error('No user found');
+  }
+
+  const matches = await bcrypt.compare(value.password, user.password);
+
+  if (!matches) {
+    throw new Error('Email and password do not match');
+  }
+
+  const token = jwt.sign({ userId: user._id }, APP_SECRET);
+
+  return {
+    token,
+    user: {
+      _id: user._id,
+      email: user.email
+    }
+  };
+}
+
 export default {
   Mutation: {
-    signup
+    signup,
+    login
   }
 };
